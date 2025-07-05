4b937b1c781fc2506d686da29250b13c5d4fb895
Manmohan Shukla <manmohanshukla815@gmail.com>
2025-07-05 14:15:00 +0530

    linting_checked

--- apps/my-app/app/canvas/[roomId]/page.tsx
+++ apps/my-app/app/canvas/[roomId]/page.tsx
@@ -1,11 +1,6 @@
 import { RoomCanvas } from "@/components/RoomCanvas";
 
-export default async function CanvasPage({
-  params,
-}: {
-  params: { roomId: string };
-}) {
-  const roomId = params.roomId;
-  return <RoomCanvas roomId={roomId} />;
+export default function CanvasPage({ params }: { params: { roomId: string } }) {
+  return <RoomCanvas roomId={params.roomId} />;
 }
 //fabric.js
