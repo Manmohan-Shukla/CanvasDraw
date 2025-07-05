9a13ec84681433e7ab0d89bbc16c4aa81add5632
Manmohan Shukla <manmohanshukla815@gmail.com>
2025-07-05 18:23:17 +0530

    links

---
+++ apps/my-app/config.ts
@@ -0,0 +1,4 @@
+export const BACKEND_URL = "http://localhost:3001";
+export const WS_URL =
+  "ws://localhost:8080?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3MWNiZTZmYi00ZjEwLTRkNjYtODI5Ny0xN2QwMTlhNDgyMDYiLCJpYXQiOjE3NTEzMTU0MTR9.EgvR8mjb0kS9SWiJSzXBWAbH5QjXMzndUjAsxxLkXHs";
+export const FRONTEND_URL = "https://canvas-draw-my-app.vercel.app/";
