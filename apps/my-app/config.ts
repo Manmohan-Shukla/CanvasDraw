a6988baca84082988bdb7f7b64d050dccb8ec0bc
Manmohan Shukla <manmohanshukla815@gmail.com>
2025-07-09 20:08:56 +0530

    added_delete-route

--- apps/my-app/config.ts
+++ apps/my-app/config.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL = "http://localhost:3001";
 export const WS_URL = "ws://localhost:8080?token=";
-// export const FRONTEND_URL = "https://canvas-draw-my-/app.vercel.app/";
-export const FRONTEND_URL = "http://localhost:3000/";
+export const FRONTEND_URL = "https://canvas-draw-my-/app.vercel.app/";
+// export const FRONTEND_URL = "http://localhost:3000/";
